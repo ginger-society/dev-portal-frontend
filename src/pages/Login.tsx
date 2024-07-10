c74494cc0b9da538aafe2d9088edbd038a7a0fe9
Ginger Society Admin <admin@gingersociety.org>
2024-07-10 12:50:54 +0530

    fix: dark and light theme for login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { AuthContext } from "@/shared/AuthContext";
 import { auth } from "@/shared/firebase";
 import router from "@/shared/router";
-import { Button, ButtonType, Input } from "@ginger-society/ginger-ui";
+import { Button, ButtonType, Input, Text } from "@ginger-society/ginger-ui";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -40,7 +40,7 @@ export const Login = () => {
       <div className="login-form-container">
         <div className="app-details-container">
           <img width={200} src="/ginger_icon.png" />
-          <h1>GingerDB Studio</h1>
+          <Text>GingerDB Studio</Text>
         </div>
         <Input label="Email" onChange={(e) => setEmail(e.target.value)} />
         <Input
@@ -52,6 +52,7 @@ export const Login = () => {
         <span className="text-error">{error}</span>
         <div className="btn-group">
           <Button label="Sign in" type={ButtonType.Primary} onClick={signIn} />
+          <Text>Or</Text>
           <Button label="Sign Up" onClick={signUp} />
         </div>
       </div>
