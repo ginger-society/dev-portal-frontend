3468f0f938352328c80b097b64b5ed6e1fe8ce3c
Ginger Society Admin <admin@gingersociety.org>
2024-07-14 19:08:32 +0530

    feat: cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./app.css";
-import "./index.css";
 import "../node_modules/@ginger-society/ginger-ui/dist/esm/index.css";
 
 import { AuthProvider } from "./shared/AuthContext";
@@ -20,5 +19,5 @@ root.render(
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
-  </StrictMode>,
+  </StrictMode>
 );
