0bd65ed78b82b257440cfa6db0845dad2f477073
Ginger Society Admin <admin@gingersociety.org>
2026-06-04 23:29:56 +0530

    feat: using gitter service instead of ginger kube

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,3 @@
 export { default as IAMService } from './IAMService_client'
 export { default as MetadataService } from './MetadataService_client'
-export { default as GingerKubeService } from './GingerKubeService_client'
+export { default as GingerGitter } from './ginger-gitter_client'
